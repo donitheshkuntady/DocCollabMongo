38d92a981d49caf046be50e845262e340b2d376e
Nithesh Kuntady <nithesh.kuntady@dreamorbit.com>
2025-03-19 15:01:40 +0530

    code add

---
+++ DocCollabMongoCore/Entities/DocumentCollabMaster.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocCollabMongoCore.Entities;
+public class DocumentCollabMaster
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    [BsonIgnoreIfDefault]
+    public string Id { get; set; }
+    public string RoomName { get; set; }
+    public string? StorageIdentifier { get; set; }
+    public int Version { get; set; }
+    public bool IsActive { get; set; }
+    public DateTimeOffset CreatedDate { get; set; }
+    public string? CreatedByUserId { get; set; }
+    public DateTimeOffset LastModifiedDate { get; set; }
+    public string? LastModifiedByUserId { get; set; }
+}
---
+++ DocCollabMongoCore/Entities/EntityContext.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using DocCollabMongoCore.Entities;
+using MongoDB.Driver;
+
+#nullable disable
+
+namespace Aca.DeliverableGenerator.Entities;
+
+public partial class EntityContext
+{
+    public IMongoDatabase Database { get; }
+    public EntityContext(IMongoDatabase database) => Database = database;
+    public IMongoCollection<DocumentCollabMaster> DocumentCollabMaster => Database.GetCollection<DocumentCollabMaster>(CollectionNames.DocumentCollabMasterCollection);
+}
+
+public static class CollectionNames
+{
+    public const string DocumentCollabMasterCollection = "DocumentCollabMaster";
+}
+
